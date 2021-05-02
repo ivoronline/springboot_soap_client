84b0ef53ff06b849c72aca1792ac21dea3ca3c74
igoreta <ivoronline@gmail.com>
2021-05-02 11:51:15 +0200

    Initial commit

---
+++ src/main/java/com/ivoronline/springboot_soap_client/SpringbootSoapClientApplication.java
@@ -0,0 +1,13 @@
+package com.ivoronline.springboot_soap_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSoapClientApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootSoapClientApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/ivoronline/springboot_soap_client/SpringbootSoapClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivoronline.springboot_soap_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSoapClientApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
